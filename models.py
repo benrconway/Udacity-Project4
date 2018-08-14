c12b64b3c6c0228f3635d2cb1a9c10388ff19051
benrconway84 <benrconway84@gmail.com>
2018-08-14 07:58:50 +0100

    Added initial structure to models.py

--- models.py
+++ models.py
@@ -1,1 +1,73 @@
-# Here I will place my models for the website and API service.
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+    password_hash = Column(String(250), nullable=false)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(Users)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    description = Column(String(250))
+    location = Column(String(250))
+    url = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(Users)
+    category = relationship(Categories)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'location': self.location,
+            'url': self.url,
+            'user_id': self.user_id,
+            'category_id': self.category_id
+        }
+
+engine = create_engine('sqlite:///itemcatalogue.db')
+
+Base.metadata.create_all(engine)
---
+++ seed_data.py
@@ -0,0 +1,1 @@
+# Here will go all the seed data to give the website something to display.
