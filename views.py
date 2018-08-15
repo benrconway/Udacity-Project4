4f16f9ee3e8f5c2d17bd2c013461d64b5d6756a3
benrconway84 <benrconway84@gmail.com>
2018-08-15 08:12:17 +0100

    Added basic JSON endpoints

--- views.py
+++ views.py
@@ -27,6 +27,8 @@ app = Flask(__name__)
 @app.route('/login')
 @app.route('oauth/<str:provider>', methods=['POST'])
 @app.route('/token')
+@auth.login_required
+
 
 @app.route('/')
 @app.route('/catalogue', methods=['GET', 'POST'])
@@ -34,7 +36,28 @@ app = Flask(__name__)
 @app.route('/catalogue/<int:catalogue_id>/items', methods=['GET', 'POST'])
 @app.route('/catalogue/<int:catalogue_id>/items/<int:item_id>/', methods=['GET', 'PUT', 'DELETE'])
 
-@app.route('/api/catalogue')
-@app.route('/api/catalogue/<int:catalogue_id>')
+@app.route('/api/catalogue/all')
+def apiMain():
+    catalogues = session.query(Catalogues).all()
+    items = session.query(Items).all()
+    return jsonify(Catalogues=[catalog.serialize for catalog in catalogues],
+        Items=[item.serialize for item in items])
+
+
+# Maybe don't need this one
+# @app.route('/api/catalogue/<int:catalogue_id>')
+# def apiCatalogueItems():
+
+
 @app.route('/api/catalogue/<int:catalogue_id>/items')
+def catalogueAndItems():
+    catalogue = session.query(Catalogues).filter_by(id=catalogue_id).one()
+    items = session.query(Items).filter_by(catalogue_id=catalogue_id).all()
+    return jsonify(Catalogues=[catalogue.serialize],
+        Items=[item.serialize for item in items])
+
+
 @app.route('/api/catalogue/<int:catalogue_id>/items/<int:item_id>')
+def singleItem():
+    item = session.query(Items).filter_by(id=item_id).one()
+    return jsonify(Items=[item.serialize])
