9e683cc9eea0338c2bffba5e01b882bd3ca06921
benrconway84 <benrconway84@gmail.com>
2018-08-23 08:53:01 +0100

    API routes work as expected

--- views.py
+++ views.py
@@ -47,11 +47,18 @@ def showOneCategoryAndItems(category_name):
 @app.route('/categories/<string:category_name>/items/new', methods=['GET', 'POST'])
 def newItem(category_name):
     category = session.query(Categories).filter_by(name=category_name).one()
-    print request
     if request.method == 'POST':
-        # TODO: create object and store it in the database
+        name = request.form['name']
+        description = request.form['description']
+        location = request.form['location']
+        url = request.form['url']
+        item = Items(name=name, description=description, location=location, url=url, category=category)
+        session.add(item)
+        session.commit()
+        # TODO: Add user from login_session
         flash("{0} has been added to {1}.".format(item.name, category.name))
-        return redirect(url_for('singleCategory.html'))
+        return redirect(url_for('showOneCategoryAndItems', category_name=category_name))
+
     else:
         return render_template('newItem.html', category=category)
 
@@ -61,13 +68,18 @@ def singleItem(category_name, item_id):
     category = session.query(Categories).filter_by(name=category_name).one()
     item = session.query(Items).filter_by(id=item_id).one()
     if request.method =='PUT':
-        # TODO: Update object in database
+        updatedItem = updateItem(request.form, item)
+        session.add(updatedItem)
+        session.commit()
         flash("Changes have been successfully made to {0}.".format(item.name))
-        return redirect(url_for())
+        return redirect(url_for('showOneCategoryAndItems', category_name=category_name))
+
     elif request.method == 'DELETE':
-        # TODO: Delete object from database
+        session.delete(item)
+        session.commit()
         flash("{0} has been deleted from {1}.".format(item.name, category.name))
-        return redirect(url_for('singleCategory.html'))
+        return redirect(url_for('showOneCategoryAndItems', category_name=category_name))
+
     else:
         return render_template('singleItem.html', category=category, item=item)
 
@@ -93,17 +105,31 @@ def apiMain():
 
 @app.route('/api/categories/<string:category_name>/items')
 def apiCatagoryAndItems(category_name):
-    category = session.query(Categories).filter_by(id=category_id).one()
-    items = session.query(Items).filter_by(category_id=category_id).all()
-    return jsonify(Categories=[catalogue.serialize],
+    category = session.query(Categories).filter_by(name=category_name).one()
+    items = session.query(Items).filter_by(category_id=category.id).all()
+    return jsonify(Categories=[category.serialize],
         Items=[item.serialize for item in items])
 
 
-@app.route('/api/category/<string:category_name>/items/<int:item_id>')
+@app.route('/api/categories/<string:category_name>/items/<int:item_id>')
 def apiSingleItem(category_name, item_id):
     item = session.query(Items).filter_by(id=item_id).one()
     return jsonify(Items=[item.serialize])
 
+# Helper functions
+def updateItem(form, item):
+    if form['name']:
+        item.name = form['name']
+    if form['description']:
+        item.description = form['description']
+    if form['location']:
+        item.location = form['location']
+    if form['url']:
+        item.url = form['url']
+    print item.location
+    return item
+
 if __name__ == '__main__':
+    app.secret_key = "password"
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
