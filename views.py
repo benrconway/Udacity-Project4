0eb23a7519fb5dba4ea74c3d37fad2052cc86f1e
benrconway84 <benrconway84@gmail.com>
2018-09-05 08:47:58 +0100

    Login page cleaned up

--- views.py
+++ views.py
@@ -41,6 +41,7 @@ loginLabel = {'login':{
                 }
             }
 
+
 @auth.verify_password
 def verifyPassword(identifier, password):
     print "Verify Password is called"
@@ -61,7 +62,7 @@ def returnToHome():
 
 @app.route('/oauth/<string:provider>', methods=['POST'])
 def loginWithOauth(provider):
-    print "Oauth endpoint called"
+    # print "Oauth endpoint called"
     requestData = json.loads(request.data)
     oneTimeCode = requestData['data']
     if provider == 'google':
@@ -74,7 +75,7 @@ def loginWithOauth(provider):
 
         except FlowExchangeError:
             return respondWith('Failed to authorize with Google', 401)
-        print "Made it past check 1"
+
         # test token validity
         googleAccessToken = userCredentials.access_token
         url = 'https://www.googleapis.com/oauth2/v1/tokeninfo'
@@ -102,7 +103,7 @@ def loginWithOauth(provider):
         # if it doesn't work, separate request and .json()
         data = requests.get(userinfoURL, params=params).json()
         print data
-        name = data['name']
+        name = data.get('name', None)
         email = data['email']
 
         # check to see if they already exist.
@@ -178,10 +179,12 @@ def logout():
 @app.route('/')
 @app.route('/categories/')
 def home():
+    user = login_session.get('user', None)
+    # user = session.merge(user)
     categories = session.query(Categories).all()
     category = Categories()
     items = session.query(Items).all()
-    if login_session['user'] is None:
+    if user is None:
         return render_template('categories.html', categories=categories,
                            items=items, category=category,
                            login=loginLabel['login'])
@@ -194,8 +197,8 @@ def home():
 @app.route('/categories/<string:category_name>/')
 @app.route('/categories/<string:category_name>/items')
 def showOneCategoryAndItems(category_name):
-    user = login_session['user']
-    user = session.merge(user)
+    user = login_session.get('user', None)
+
     category = session.query(Categories).filter_by(name=category_name).one()
     items = session.query(Items).filter_by(category_id=category.id).all()
     if user is None:
@@ -203,7 +206,7 @@ def showOneCategoryAndItems(category_name):
                                 category=category, items=items,
                                 login=loginLabel['login'])
     else:
-        print
+        user = session.merge(user)
         return render_template('singleCategory.html',
                                category=category, items=items, user=user,
                                login=loginLabel['logout'])
@@ -212,7 +215,7 @@ def showOneCategoryAndItems(category_name):
 @app.route('/categories/<string:category_name>/items/new',
            methods=['GET', 'POST'])
 def newItem(category_name):
-    user = login_session['user']
+    user = login_session.get('user', None)
     category = session.query(Categories).filter_by(name=category_name).one()
     # User must be logged in to create an item.
     if user is None:
@@ -238,7 +241,7 @@ def newItem(category_name):
 
 @app.route('/categories/<string:category_name>/items/<int:item_id>/')
 def singleItem(category_name, item_id):
-    user = login_session['user']
+    user = login_session.get('user', None)
     category = session.query(Categories).filter_by(name=category_name).one()
     item = session.query(Items).filter_by(id=item_id).one()
 
@@ -256,7 +259,7 @@ def singleItem(category_name, item_id):
 @app.route('/categories/<string:category_name>/items/<int:item_id>/edit',
            methods=['GET', 'POST'])
 def editItem(category_name, item_id):
-    user = login_session['user']
+    user = login_session.get('user', None)
     category = session.query(Categories).filter_by(name=category_name).one()
     item = session.query(Items).filter_by(id=item_id).one()
     if user is None:
@@ -290,8 +293,8 @@ def editItem(category_name, item_id):
 @app.route('/categories/<string:category_name>/items/<int:item_id>/delete',
            methods=['GET', 'POST'])
 def deleteItem(category_name, item_id):
-    user = login_session['user']
-    user = session.merge(user)
+    user = login_session.get('user', None)
+
     category = session.query(Categories).filter_by(name=category_name).one()
     item = session.query(Items).filter_by(id=item_id).one()
     # If you aren't logged in, redirect to login.
@@ -300,6 +303,7 @@ def deleteItem(category_name, item_id):
         return redirect(url_for('login'))
 
     if request.method == 'POST':
+        user = session.merge(user)
         # If posting to delete an object, check the IDs match.
         if user.id != item.user_id:
             flash("You do not have authority to delete {0}.".format(item.name))
